96edd23aad689ae86cad5513d84e75686ecd5c5b
revrenlove <58348549+revrenlove@users.noreply.github.com>
2024-05-01 18:17:36 -0500

    1 typedocjsdoc (#6)
    * Update gitignore for `example/`
    
    * Adding jsdox
    
    * Documentation

--- src/command-id.ts
+++ src/command-id.ts
@@ -1,7 +1,10 @@
-export function commandId(id: string) {
+/**
+ * @param command - A unique identifier for the command. Should match the contribution point in `package.json`.
+ */
+export function commandId(command: string) {
     return function <T extends { new(...args: any[]): {} }>(constructor: T) {
         return class extends constructor {
-            static commandId: string = id;
+            static commandId: string = command;
         };
     };
 }
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import { CommandBase } from "./command-base";
-import { ContextCommandBase } from './context-command-base';
 import { commandId } from './command-id';
 import { registerCommands } from './register-commands';
 
-export { registerCommands, CommandBase, ContextCommandBase, commandId };
+export {
+    CommandBase,
+    commandId,
+    registerCommands,
+};
